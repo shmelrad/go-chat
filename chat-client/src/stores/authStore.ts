d0dbaa505924c0770a71d2cce65792c5ee5303c0
shmelrad <shmelrad@gmail.com>
2025-01-15 22:59:11 +0300

    correctly decode user from token

--- chat-client/src/stores/authStore.ts
+++ chat-client/src/stores/authStore.ts
@@ -9,19 +9,37 @@ interface AuthState {
   logout: () => void
 }
 
+interface DecodedUser {
+  sub: string;
+  username: string;
+  email: string;
+}
+
+const decodeUser = (token: string) => {
+  const userInfo = jwtDecode<DecodedUser>(token)
+  return {
+    id: parseInt(userInfo.sub),
+    username: userInfo.username,
+    email: userInfo.email
+  }
+}
+
 export const useAuthStore = create<AuthState>((set) => {
   const token = localStorage.getItem('token')
-  const userInfo = token ? jwtDecode<User>(token) : null
+  const user = token ? decodeUser(token) : null
+
   return {
     token,
     login: (token) => {
       localStorage.setItem('token', token)
-      set({ token })
+      const user = decodeUser(token)
+      set({ token, user })
     },
-    user: userInfo,
+    user,
     logout: () => {
       localStorage.removeItem('token')
-      set({ token: null })
+      set({ token: null, user: null })
     }
   }
 })
+
