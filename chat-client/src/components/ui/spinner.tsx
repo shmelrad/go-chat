53690490686b46e5b7da1f915ac3d83fcff3f1d2
shmelrad <shmelrad@gmail.com>
2025-01-15 22:55:21 +0300

    impl spinner component

---
+++ chat-client/src/components/ui/spinner.tsx
@@ -0,0 +1,3 @@
+export default function Spinner() {
+    return <div className="size-6 animate-spin rounded-full border-2 border-t-primary border-b-transparent border-l-transparent border-r-transparent"></div>
+}
