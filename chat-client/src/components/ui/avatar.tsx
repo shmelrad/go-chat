159011bfac37d1b7843d726cba1c9856a6ed84ce
shmelrad <shmelrad@gmail.com>
2025-01-17 01:36:59 +0300

    improve light theme

--- chat-client/src/components/ui/avatar.tsx
+++ chat-client/src/components/ui/avatar.tsx
@@ -37,7 +37,7 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-violet-700",
+      "flex h-full w-full items-center justify-center rounded-full bg-violet-700 text-white",
       className
     )}
     {...props}
