6992af28ddd2325c7c509c0afbc04790f17d7dd9
shmelrad <shmelrad@gmail.com>
2025-01-28 08:28:27 +0300

    remove deleted_at field from base model

--- chat-server/models/base_model.go
+++ chat-server/models/base_model.go
@@ -2,13 +2,10 @@ package models
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type BaseModel struct {
 	ID        uint `json:"id" gorm:"primarykey"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }
